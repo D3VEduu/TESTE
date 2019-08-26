8d2943afad433313060035c7da69be4913a8817e
D3VEduu <edu.ardo0123@hotmail.com>
2019-08-26 01:23:57 -0300

    Inicio aula JS

---
+++ fundamentos/math.js
@@ -0,0 +1,5 @@
+const radio = 5.6
+const area = Math.PI * Math.pow(radio, 2)
+
+console.log(area.toFixed()) //VALOR -> 99
+console.log(typeof Math) //OBJETO
---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0) //VALOR INFINITO 
+//VALOR NUMERO -> 5
+console.log("10" / 2)
+console.log("Show" * 2) //NOT A NUMBER -> NaN
+console.log(0.1 + 0.7) //IMPRECISAO DOS DADOS
+//console.log(10.toString())
+console.log(10.345.toFixed(2)) //VALOR -> 10.35
